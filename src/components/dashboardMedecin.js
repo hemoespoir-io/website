aab009ca8f4e271cbd974b8f27ccb7033503cc2c
Nabil Qasmi <nabil.qasmi1@gmail.com>
2024-08-22 10:22:00 +0200

    feat-13

--- src/components/dashboardMedecin.js
+++ src/components/dashboardMedecin.js
@@ -34,7 +34,7 @@ const CalendarPage = () => {
 
     const fetchAppointments = async () => {
       try {
-        const response = await fetch('http://127.0.0.1:5000/getAppointmentM', {
+        const response = await fetch(`${config.BACKEND_URL}getAppointmentMedecin`, { 
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
